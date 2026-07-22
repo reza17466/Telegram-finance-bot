144fe5368c5df81b8f19d065ba0b96d3d075c635
reza17466 <rezachash12@gmail.com>
2026-07-22 19:33:24 +0330

    Update Main.py

--- Main.py
+++ Main.py
@@ -67,4 +67,6 @@ if __name__ == '__main__':
         app.add_handler(CommandHandler("start", start))
         app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message))
         print("ربات روشن شد...")
+        keep_alive()
+# خط بعدی همون دستور استارت قبلی خودت باشه
         app.run_polling()
